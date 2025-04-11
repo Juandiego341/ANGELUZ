cebdee155e0c2266400722e857effa3ad7a7e00b
Juandiego341 <betancurd913@gmail.com>
2025-04-11 16:29:05 -0500

    se monta estructur basica de Angeluz

---
+++ src/main/java/com/juan/springboot/angeluz/AngeluzApplication.java
@@ -0,0 +1,13 @@
+package com.juan.springboot.angeluz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngeluzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngeluzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan/springboot/angeluz/AngeluzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.springboot.angeluz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngeluzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
